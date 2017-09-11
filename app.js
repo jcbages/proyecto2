50434bd318bde37ecaeaa4bb3032ef8f61d4e77a
Carlos Gonzalez <ce.gonzalez13@uniandes.edu.co>
2017-09-11 01:54:14 -0500

    Example CRUD working, and now the list only updates when AJAX call finishes completely

--- app.js
+++ app.js
@@ -49,10 +49,9 @@ app.post("/add", function(req, res){
 
   MongoClient.connect(url,function(err,db){
     if( err) throw err;
-    db.collection("personas").insertOne(item,function(err){
-      if(err) throw err;
+    db.collection("personas").insertOne(item).then(function(){
+      res.sendStatus(200);
       db.close();
-      res.sendView("views/index.pug");
     });
   });
 });
@@ -62,21 +61,45 @@ app.get("/add", function(req, res){
     if( err) throw err;
     db.collection("personas").find({}).toArray(function(error, documents) {
       if (err) throw error;
-
       res.send(documents);
     });
   });
 });
+
 app.delete("/delete/:id", function (req, res) {
   var id = req.params.id;
   console.log(id);
   MongoClient.connect(url,function(err,db){
     if( err) throw err;
-    db.collection("personas").remove({"_id" : ObjectId(id)});
-    db.close();
-    res.sendView("views/index.pug");
-  });
+    db.collection("personas").remove({"_id" : ObjectId(id)}).then(function(){
+      res.sendStatus(200);
+      db.close();
 
+    });
+  });
+});
+app.get("/update/:id",function(req,res){
+  var id = req.params.id;
+  MongoClient.connect(url,function(err,db){
+    if( err) throw err;
+    db.collection("personas").findOne({"_id" : ObjectId(id)}).then(function(pers){
+      res.render("updatePersona",{persona:pers});
+      db.close();
+    });
+  });
+});
+app.put("/update/:id",function(req,res){
+  var id = req.params.id;
+  var name = req.body.pname;
+  var email = req.body.pEmail;
+  var number = req.body.pNumber;
+  MongoClient.connect(url,function(err,db){
+    if( err) throw err;
+    db.collection("personas").update({"_id" : ObjectId(id)},{name:name, email:email,number:number},{ upsert: true }).then(function(){
+      res.sendStatus(200);
+      db.close();
+    });
+  });  
 });
 
 app.listen(8000);
--- views/index.js
+++ views/index.js
@@ -1,40 +1,56 @@
 $(document).ready( function () {
-  $("form").submit( function () {
+  $("#addForm").submit( function () {
     var formdata = $(this).serialize();
-    $("form").trigger("reset");
-    window.location.reload();
+    $("#addForm").trigger("reset");
     $.ajax({
       type: "POST",
       url: "/add",
       data: formdata,
+    }).done(function(){
+      window.location.reload();
     });
     return false;
   });
   $(".delete").click( function () {
-    var formdata = $(this).serialize();
     var id = $(".delete").attr("id").split("-")[1];
     window.location.reload();
     $.ajax({
       url: "delete/"+ id,
       type: "DELETE",
+    }).done(function(){
+      window.location.replace("/");
     });
     return false;
   });
+  $(".update").click( function () {
+    var formdata = $(".updateForm").serialize();
+    var id = $(".update").attr("id").split("-")[1];
+    $.ajax({
+      type: "PUT",
+      url: "/update/"+id,
+      data: formdata,
+    }).done(function(){
+      window.location.replace("/");
+    });
+    return false;
+  });
+
+
   $(".star").on("click", function () {
     $(this).toggleClass("star-checked");
   });
 
-  $(".ckbox label").on('click', function () {
-    $(this).parents('tr').toggleClass('selected');
+  $(".ckbox label").on("click", function () {
+    $(this).parents("tr").toggleClass("selected");
   });
 
-  $('.btn-filter').on('click', function () {
-    var $target = $(this).data('target');
-    if ($target != 'all') {
-      $('.table-filter tr').css('display', 'none');
-      $('.table-filter tr[data-status="' + $target + '"]').fadeIn('slow');
+  $(".btn-filter").on("click", function () {
+    var $target = $(this).data("target");
+    if ($target != "all") {
+      $(".table-filter tr").css("display", "none");
+      $(".table-filter tr[data-status='" + $target + "']").fadeIn("slow");
     } else {
-      $('.table tr-filter').css('display', 'none').fadeIn('slow');
+      $(".table tr-filter").css("display", "none").fadeIn("slow");
     }
   });
 
